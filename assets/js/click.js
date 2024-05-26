5550937ffbc5b0336e197944bdd38eb8cabed72c
hieuxau123 <teamlienquan1@gmail.com>
2024-05-26 20:52:18 +0700

    first commit

---
+++ assets/js/click.js
@@ -0,0 +1,297 @@
+import { dogs, cats, foods_pet, benefits } from "./product.js";
+
+let index = 0;
+let totalQuantity = 0;
+
+// Bắt sự kiện khi người dùng thực hiện cuộn trang
+window.addEventListener("scroll", function () {
+  var header = this.document.getElementById("header");
+  var scrollPosition = this.window.scrollY;
+
+  if (scrollPosition > 900) {
+    header.classList.add("open");
+  } else {
+    header.classList.remove("open");
+  }
+});
+// SLIDER AUTO
+const sliderContents = document.querySelectorAll(".slider .slider-content");
+function slideAuto() {
+  index = index + 1;
+  if (index > sliderContents.length - 1) {
+    index = 0;
+  }
+  sliderContents.forEach((content, i) => {
+    if (i === index) {
+      content.style.transform = `translateX(0)`;
+    } else {
+      content.style.transform = `translateX(-100%)`;
+    }
+  });
+}
+setInterval(slideAuto, 5000);
+
+// ============================= list-dog ==================================
+const dogsElement = document.querySelector(".list_dog");
+
+let dogsHtml = dogs.map((element) => {
+  return `<li>
+  <div class="card-pet" data-id="${element.id}">
+    <div class="imagine">
+      <img src="${element.image}" alt="" />
+      <div class="card-icon">
+        <strong class="click-button">+</strong>
+      </div>
+    </div>
+    <div class="info_pet">
+      <div class="title_pet">
+        <p>${element.type}</p>
+        <p class="name">${element.name}</p>
+      </div>
+      <span class="price">${element.price}</span>
+    </div>
+  </div>
+</li>
+`;
+});
+
+dogsElement.innerHTML = dogsHtml.join("");
+
+// ============================= list-cat ==================================
+const catsElement = document.querySelector(".list_cat");
+
+let catsHtml = cats.map((element) => {
+  return `<li>
+    <div class="card-pet" data-id="${element.id}">
+      <div class="imagine">
+        <img src="${element.image}" alt="" />
+        <div class="card-icon">
+          <strong class="click-button">+</strong>
+        </div>
+      </div>
+      <div class="info_pet">
+        <div class="title_pet">
+          <p>${element.type}</p>
+          <p class="name">${element.name}</p>
+        </div>
+        <span class="price">${element.price}</span>
+      </div>
+    </div>
+  </li>
+`;
+});
+
+catsElement.innerHTML = catsHtml.join("");
+
+// ==================list-food_pet=============================
+const food_petsElement = document.querySelector(".list_food_pet");
+
+let food_petsHtml = foods_pet.map((element) => {
+  return `<li>
+    <div class="card-pet" data-id="${element.id}">
+      <div class="imagine">
+        <img src="${element.image}" alt="" />
+        <div class="card-icon">
+          <strong class="click-button">+</strong>
+        </div>
+      </div>
+      <div class="info_pet">
+        <div class="title_pet">
+          <p>${element.type}</p>
+          <p class="name">${element.name}</p>
+        </div>
+        <span class="price">${element.price}</span>
+      </div>
+    </div>
+  </li>
+`;
+});
+food_petsElement.innerHTML = food_petsHtml.join("");
+
+//BENEFITS-LIST=========================================================
+const benefitsElement = document.querySelector(".benefit-lists");
+
+let benefitsHtml = benefits.map((element) => {
+  return ` <li class="boxs">
+    <div class="box">
+      <div class="box-img">
+        <img src="${element.image}" alt="" />
+      </div>
+      <div class="box-title">
+        <h5>
+        ${element.title}
+        </h5>
+        <p>
+        ${element.description}
+        </p>
+        <button>Xem thêm</button>
+      </div>
+    </div>
+  </li>`;
+});
+
+benefitsElement.innerHTML = benefitsHtml.join("");
+
+//========================LIST-BENEFITS- FUNCTION=======================
+const btnNext = document.querySelector(".next");
+const btnPrev = document.querySelector(".prev");
+const benefit_list = document.querySelector(".benefit-lists");
+// Số lượng ảnh hiện thị trên mỗi slide
+const numItemsPerPage = 3;
+
+// Số lượng ảnh trong danh sách
+const numItems = benefit_list.children.length;
+
+// Kích thước của mỗi ảnh (giả sử ảnh có cùng kích thước)
+const itemWidth = benefit_list.children[0].offsetWidth;
+// Số lượng slide
+const numSlides = Math.ceil(numItems / numItemsPerPage);
+
+let currentIndex = 0; // Vị trí hiện tại của slide
+
+// Hàm di chuyển slide sang phải
+function slideRight() {
+  if (currentIndex < numSlides - 1) {
+    currentIndex++;
+  } else {
+    currentIndex = 0; // Quay lại slide đầu tiên nếu đã đạt tới slide cuối cùng
+  }
+  if (numItems - currentIndex <= 3) {
+    currentIndex = 0; // Quay lại ảnh ban đầu
+  }
+  updateSlidePosition();
+}
+
+// Hàm di chuyển slide sang trái
+function slideLeft() {
+  if (currentIndex > 0) {
+    currentIndex--;
+  }
+  updateSlidePosition();
+}
+
+// Hàm cập nhật vị trí của slide
+function updateSlidePosition() {
+  benefit_list.style.transform = `translateX(${
+    -3 * currentIndex * itemWidth
+  }px)`;
+}
+
+// Gán sự kiện click cho nút arrow
+btnNext.addEventListener("click", slideRight);
+btnPrev.addEventListener("click", slideRight);
+// =============================================================================================================
+
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", ready);
+} else {
+  ready();
+}
+
+//Making function
+function ready() {
+  // Remove Item From cart
+  var removeCartButtons = document.querySelectorAll(".cart-remov");
+  for (var i = 0; i < removeCartButtons.length; i++) {
+    var button = removeCartButtons[i];
+    button.addEventListener("click", removeCartItem);
+  }
+
+  //Quantity changes
+  var quantityInput = document.getElementsByClassName("cart-quantity");
+  console.log(quantityInput);
+  for (var i = 0; i < quantityInput.length; i++) {
+    var input = quantityInput[i];
+    input.addEventListener("change", quantityChanged);
+  }
+
+  //Add To Cart
+  var addCart = document.querySelectorAll(".click-button");
+  for (var i = 0; i < addCart.length; i++) {
+    var button = addCart[i];
+    button.addEventListener("click", addCartClicked);
+  }
+}
+//Remove item from cart
+function removeCartItem(event) {
+  var buttonClicked = event.target;
+  buttonClicked.parentElement.remove();
+  updateTotal();
+  numberCart();
+}
+// Quantity change
+function quantityChanged(event) {
+  var input = event.target;
+
+  if (isNaN(input.value) || input.value <= 0) {
+    input.value = 1;
+  }
+  updateTotal();
+}
+
+// AddCart
+function addCartClicked(event) {
+  var button = event.target;
+  var shopProducts = button.closest(".card-pet");
+  var nameProduct = shopProducts.querySelector(".name").innerHTML;
+  var priceProduct = shopProducts.querySelector(".price").innerHTML;
+  var imgProduct = shopProducts.querySelector(".imagine img").src;
+  addCartToHtml(nameProduct, priceProduct, imgProduct);
+}
+
+function addCartToHtml(name, price, img) {
+  var item = document.createElement("li");
+  item.classList.add("item");
+
+  var cartItems = document.getElementsByClassName("list-buy")[0];
+  var cartItemsNames = cartItems.getElementsByClassName("name");
+  for (var i = 0; i < cartItemsNames.length; i++) {
+    if (cartItemsNames[i].innerHTML == name) return;
+  }
+  var itemContent = `<div class="image-item">
+              <img src="${img}" alt="" />
+              </div>
+              <div class="detail-item">
+              <div class="name">${name}</div>
+              <div class="price">${price}</div>
+              <input type="number" value="1" class="cart-quantity" />
+              </div>
+              <i class="bx bxs-trash-alt cart-remov"></i>`;
+  item.innerHTML = itemContent;
+  cartItems.appendChild(item);
+  item.querySelector(".cart-remov").addEventListener("click", removeCartItem);
+  item
+    .querySelector(".cart-quantity")
+    .addEventListener("change", quantityChanged);
+  updateTotal();
+}
+function numberCart() {
+  var numBlockCart = document.querySelector(".number-buy");
+  if (totalQuantity > 0) {
+    numBlockCart.textContent = totalQuantity;
+    numBlockCart.style.visibility = "visible";
+  }else
+  numBlockCart.style.visibility = "hidden";
+}
+
+function updateTotal() {
+  var listBuy = document.getElementsByClassName("list-buy")[0];
+  var cartBoxes = listBuy.getElementsByClassName("item");
+  var total = 0;
+  totalQuantity = 0;
+
+  for (var i = 0; i < cartBoxes.length; i++) {
+    var cartBox = cartBoxes[i];
+    var priceElement = cartBox.querySelector(".price");
+    var quantityElement = cartBox.querySelector(".cart-quantity");
+    var price = parseFloat(priceElement.innerHTML.replace("$", ""));
+    var quantity = quantityElement.value;
+    total = total + price * quantity;
+    total = Math.round(total * 100) / 100;
+
+    totalQuantity += parseInt(quantity);
+    document.querySelector(".total-price").innerHTML = "$" + total;
+  }
+  numberCart();
+  
+}
---
+++ assets/js/product.js
@@ -0,0 +1,217 @@
+let dogs = [
+  {
+    id: 1,
+    image: "assets/img/2 chó.jpg",
+    type: "Chó cảnh",
+    name: "Chó Doberman Pinscher",
+    price: "$1000",
+  },
+  {
+    id: 2,
+    image: "assets/img/chó-2.jpg",
+    type: "Chó cảnh",
+    name: "Chó American Eskimo",
+    price: "$2000",
+  },
+  {
+    id: 3,
+    image: "assets/img/chó-3.jpg",
+    type: "Chó cảnh",
+    name: "Chó bulldog",
+    price: "$1100",
+  },
+  {
+    id: 4,
+    image: "assets/img/chó-4.jpg",
+    type: "Chó canh phòng",
+    name: "Chó Becgier",
+    price: "$550",
+  },
+  {
+    id: 5,
+    image: "assets/img/chó-5.jpg",
+    type: "Chó cảnh",
+    name: "Đàn chó Golden 4 tháng",
+    price: "$610",
+  },
+  {
+    id: 6,
+    image: "assets/img/chó-6.jpg",
+    type: "Chó cảnh",
+    name: "Chó Doberman Pinscher",
+    price: "$700",
+  },
+  {
+    id: 7,
+    image: "assets/img/chó-7.jpg",
+    type: "Chó cảnh",
+    name: "Chó American Bully",
+    price: "$804",
+  },
+  {
+    id: 8,
+    image: "assets/img/chó-8.jpg",
+    type: "Chó cảnh",
+    name: "chó Alaska",
+    price: "$1500",
+  },
+];
+
+let cats = [
+  {
+    id: 9,
+    image: "assets/img/Mèo-1.jpg",
+    type: "Mèo cảnh",
+    name: "Mèo nhân sư không lông – Sphynx",
+    price: "$800",
+  },
+  {
+    id: 10,
+    image: "assets/img/Mèo-2.jpg",
+    type: "Mèo cảnh",
+    name: "Mèo Mỹ tai xoắn – American Curl",
+    price: "$200",
+  },
+  {
+    id: 11,
+    image: "assets/img/Mèo-3.jpg",
+    type: "Mèo cảnh",
+    name: "Mèo Nga mắt xanh – Russian Blue",
+    price: "$300",
+  },
+  {
+    id: 12,
+    image: "assets/img/Mèo-4.jpg",
+    type: "Mèo cảnh",
+    name: "Mèo Anh lông dài – British Longhair",
+    price: "$250",
+  },
+  {
+    id: 13,
+    image: "assets/img/Mèo-5.jpg",
+    type: "Mèo cảnh",
+    name: "Mèo Anh lông ngắn – British",
+    price: "$600",
+  },
+  {
+    id: 14,
+    image: "assets/img/Mèo-6.jpg",
+    type: "Mèo cảnh",
+    name: "Mèo Thổ Nhĩ Kỳ Turkish Angora",
+    price: "$550",
+  },
+  {
+    id: 15,
+    image: "assets/img/Mèo-7.jpg",
+    type: "Mèo cảnh",
+    name: "Mèo Van Thổ – Turkish Van",
+    price: "$300",
+  },
+  {
+    id: 16,
+    image: "assets/img/Mèo-8.jpg",
+    type: "Mèo cảnh",
+    name: "Mèo Ba Tư lông ngắn – Exotic",
+    price: "$330",
+  },
+];
+
+let foods_pet = [
+  {
+    id: 17,
+    image: "assets/img/đồ-1.jpg",
+    type: "ĐỒ ĂN CHO CHÓ",
+    name: "Pate Monge gà 100g",
+    price: "$3",
+  },
+  {
+    id: 18,
+    image: "assets/img/đồ-2.jpg",
+    type: "ĐỒ ĂN CHO CHÓ",
+    name: "Xương gặm sạch răng",
+    price: "$5",
+  },
+  {
+    id: 19,
+    image: "assets/img/đồ-3.jpg",
+    type: "ĐỒ ĂN CHO CHÓ",
+    name: "Mèo Nga mắt xanh – Russian Blue",
+    price: "$2",
+  },
+  {
+    id: 20,
+    image: "assets/img/đồ-4.jpg",
+    type: "ĐỒ ĂN CHO CHÓ",
+    name: "Mèo Anh lông dài – British Longhair",
+    price: "$2.5",
+  },
+  {
+    id: 21,
+    image: "assets/img/đồ-5.jpg",
+    type: "ĐỒ ĂN CHO CHÓ",
+    name: "Pate Monge cá hồi và lê 100g",
+    price: "$1",
+  },
+  {
+    id: 22,
+    image: "assets/img/đồ-6.jpg",
+    type: "ĐỒ ĂN CHO MÈO",
+    name: "Cỏ mèo tự trồng Hagen Catit",
+    price: "$10",
+  },
+  {
+    id: 23,
+    image: "assets/img/đồ-7.jpg",
+    type: "ĐỒ ĂN CHO MÈO",
+    name: "Royal Canin BabyCat",
+    price: "$25",
+  },
+  {
+    id: 24,
+    image: "assets/img/đồ-8.jpg",
+    type: "ĐỒ ĂN CHO MÈO",
+    name: "Whiskas tuna 1,2kg",
+    price: "$9",
+  },
+];
+
+let benefits = [
+  {
+    image: "assets/img/box-1.png",
+    title: "Chế độ ăn dinh dưỡng chó con",
+    description:
+      "Chế độ ăn cho chó con Chó con từ 2 tháng tuổi đến 6 tháng[...]",
+  },
+  {
+    image: "assets/img/banner-doctor.jpg",
+    title: "Tuổi thọ mèo",
+    description:
+      "Tuổi thọ trung bình của mèo nếu sống trong nhà là 13 năm. Nhưng nếu [...]",
+  },
+  {
+    image: "assets/img/box-3.jpg",
+    title: "Kinh nghiệm nuôi chó",
+    description:
+      "Kinh nghiệm nuôi chó con: Chọn chó con khỏe mạnh để nuôi Mọi người khi đi [...]",
+  },
+  {
+    image: "assets/img/box-4.jpg",
+    title: "Duis luctus elit nisi, et cursus magna pellentesque non.",
+    description:
+      "Kinh nghiệm nuôi chó con: Chọn chó con khỏe mạnh để nuôi Mọi người khi đi [...]  ",
+  },
+  {
+    image: "assets/img/box-5.jpg",
+    title: "Duis luctus elit nisi, et cursus magna pellentesque non.",
+    description:
+      "Chế độ ăn cho chó con Chó con từ 2 tháng tuổi đến 6 tháng[...]",
+  },
+  {
+    image: "assets/img/box-6.jpg",
+    title: "Duis luctus elit nisi, et cursus magna pellentesque non.",
+    description:
+      "Chế độ ăn cho chó con Chó con từ 2 tháng tuổi đến 6 tháng[...]",
+  },
+  
+];
+export { dogs, cats, foods_pet, benefits };
